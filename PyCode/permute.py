ce041f576cbc7cfcf358c36faa054fb4ffea3d51
mashihill <keeneking@gmail.com>
2015-03-30 22:59:18 +0800

    clean up permute.py

--- PyCode/permute.py
+++ PyCode/permute.py
@@ -1,9 +1,12 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
 def per(fst, lst):
     if len(lst) == 0:
         print fst
     else:
         for ele in lst:
-            per(fst+[ele], [_lst for _lst in lst if _lst not in [ele]])
+            per(fst + [ele], [_ for _ in lst if _ not in [ele]])
 
 def permute(inplist):
     per([], inplist)
